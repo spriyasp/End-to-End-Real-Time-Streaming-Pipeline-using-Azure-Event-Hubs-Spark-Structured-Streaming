d8cafffa177428015a594c6a4e0cba8999d9f677
spriyasp <116760240+spriyasp@users.noreply.github.com>
2026-05-16 17:15:53 +0530

    Add files via upload

---
+++ pythonapi/api.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from connection import send_to_event_hub, generate_uber_ride_confirmation
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def booking_home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html",
+        context={}
+    )
+
+
+@app.get("/book")
+def book_ride(request: Request):  
+    ride = generate_uber_ride_confirmation()
+    result = send_to_event_hub(ride)
+
+    return templates.TemplateResponse(
+        request=request,
+        name="confirmation.html",
+        context={}
+    )
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
